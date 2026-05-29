2de2f709376dfbe0ef538ca2c01df3c324ddb375
Jatin Sathe <jatin_anilsathe@epam.com>
2026-05-29 15:40:43 +0530

    completed email verification feature

---
+++ src/main/java/com/epam/edp/demo/dto/EmailVerificationRequestDTO.java
@@ -0,0 +1,17 @@
+package com.epam.edp.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailVerificationRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a well-formed email address")
+    private String email;
+}
